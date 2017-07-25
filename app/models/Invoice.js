a715ae77300b8da0822fe9b4db5aac2df1867fb3
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-25 16:12:29 -0300

    :poop: GET/DELETE are working, POST/PUT are not

--- app/models/Invoice.js
+++ app/models/Invoice.js
@@ -1,20 +1,52 @@
 var db = require('../db_connection_pooling');
 
 var Invoice = {
-  getAllInvoices:function(callback){
+  getAllInvoices: function(callback) {
     return db.query("Select * from Invoice", callback);
   },
-  getInvoiceById:function(id, callback){
-    return db.query("select * from Invoice where Id=?", [id], callback);
+
+  getInvoiceById: function(id, callback) {
+    return db.query("select * from Invoice where Id = ?", [id], callback);
   },
-  addInvoice:function(Invoice, callback){
-    return db.query("Insert into Invoice values(?,?)", [Invoice.ReferenceMonth, Invoice.ReferenceYear], callback);
+
+  addInvoice: function(Invoice, callback) {
+    console.log(Invoice.CreatedAt);
+    query =
+    return db.query("Insert into Invoice values(?, ?, ?, ?, ?, ?, ?)", [
+      Invoice.CreatedAt,
+      Invoice.ReferenceMonth,
+      Invoice.ReferenceYear,
+      Invoice.Document,
+      Invoice.Description,
+      Invoice.Amount,
+      Invoice.IsActive
+    ], callback);
   },
-  deleteInvoice:function(id,callback){
-    return db.query("delete from Invoice where Id=?", [id], callback);
+
+  deleteInvoice: function(id,callback) {
+    return db.query("delete from Invoice where Id = ?", [id], callback);
   },
-  updateInvoice:function(id, Invoice, callback){
-    return db.query("update Invoice set ReferenceMont=?, ReferenceYear=? where Id=?", [Invoice.ReferenceMonth, Invoice.ReferenceYear, id], callback);
+
+  updateInvoice: function(id, Invoice, callback) {
+    return db.query("update Invoice set \
+      CreatedAt = ?, \
+      ReferenceMonth = ?, \
+      ReferenceYear = ?, \
+      Document = ?, \
+      Description = ?, \
+      Amount = ?, \
+      IsActive = ?, \
+      where Id = ?",
+      [
+        Invoice.CreatedAt,
+        Invoice.ReferenceMonth,
+        Invoice.ReferenceYear,
+        Invoice.Document,
+        Invoice.Description,
+        Invoice.Amount,
+        Invoice.IsActive,
+        id
+      ], callback);
   }
 };
- module.exports=Invoice;
+module.exports=Invoice;
--- app/routes/invoices.js
+++ app/routes/invoices.js
@@ -1,24 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+var Invoice = require('../models/Invoice')
+
 /* GET invoices listing. */
 router.get('/', function(req, res, next) {
-  res.send('GET on /invoices');
+  //res.send('GET on /invoices');
+  Invoice.getAllInvoices(function(error, rows) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+/* GET single invoice. */
+router.get('/:invoiceId', function(req, res, next) {
+  //res.send('GET on /invoices with invoiceId: ' + req.params['invoiceId']);
+  Invoice.getInvoiceById(req.params.invoiceId, function(error, rows) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 /* POST invoice. */
 router.post('/', function(req, res, next) {
-  res.send('POST on /invoices')
+  //res.send('POST on /invoices')
+  Invoice.addInvoice(req.body, function(error, rows) {
+    if (error) {
+      res.json(error);
+    } else {
+      console.log(rows)
+      res.json(req.body)
+    }
+  });
 });
 
 /* PUT invoice. */
-router.put('/', function(req, res, next) {
-  res.send('PUT on /invoices')
+router.put('/:invoiceId', function(req, res, next) {
+  //res.send('PUT on /invoices')
+  Invoice.updateInvoice(req.params.invoiceId, req.body, function(error, rows) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 /* DELETE invoice. */
-router.delete('/', function(req, res, next) {
-  res.send('DELETE on /invoices')
+router.delete('/:invoiceId', function(req, res, next) {
+  //res.send('DELETE on /invoices')
+  Invoice.deleteInvoice(req.params.invoiceId, function(error, rows) {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 module.exports = router;
