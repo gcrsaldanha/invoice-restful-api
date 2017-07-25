dd1d8fb410bdc20abb407e509714c6a003f30dec
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-25 20:19:13 -0300

    :memo: Remove unnecessary files and dependencies (e.g., views, static files)

--- app/app.js
+++ app/app.js
@@ -1,31 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var cors = require('cors');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 var invoices = require('./routes/invoices');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cors())
 
-app.use('/', index);
 app.use('/users', users);
 app.use('/invoices', invoices);
 
