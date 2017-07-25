5c7519b684f72482b16216c46255ba9b05dd88f1
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-25 11:40:17 -0300

    :art: Convert all tabs -> 2 spaces

--- app/models/queries/create_invoice_table.sql
+++ app/models/queries/create_invoice_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `Invoice` (
-	`Id` INTEGER NOT NULL AUTO_INCREMENT,
-	`CreatedAt` DATETIME,
-	`ReferenceMonth` INTEGER,
-	`ReferenceYear` INTEGER,
-	`Document` VARCHAR(14),
-	`Description` VARCHAR(256),
-	`Amount` DECIMAL(16, 2),
-	`IsActive` TINYINT,
-	PRIMARY KEY(`Id`)
+  `Id` INTEGER NOT NULL AUTO_INCREMENT,
+  `CreatedAt` DATETIME,
+  `ReferenceMonth` INTEGER,
+  `ReferenceYear` INTEGER,
+  `Document` VARCHAR(14),
+  `Description` VARCHAR(256),
+  `Amount` DECIMAL(16, 2),
+  `IsActive` TINYINT,
+  PRIMARY KEY(`Id`)
 )
--- app/models/queries/dump_data.sql
+++ app/models/queries/dump_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Invoices` (`Id`, `CreatedAt`, `ReferenceMonth`, `ReferenceYear`, `Document`, `Description`, `Amount`, `IsActive`) VALUES
-	(NULL, '2017-07-11 12:30:10', '11', '1991', '1-documentfake', '1 - Dumping description etc', '10.20', '1'),
-	(NULL, '2017-07-12 12:30:10', '12', '1992', '2-documentfake', '2 - Dumping description etc', '99', '1'),
-	(NULL, '2017-07-13 12:30:10', '3', '1993', '3-documentfake', '3 - Dumping description etc', '101.20', '0');
+  (NULL, '2017-07-11 12:30:10', '11', '1991', '1-documentfake', '1 - Dumping description etc', '10.20', '1'),
+  (NULL, '2017-07-12 12:30:10', '12', '1992', '2-documentfake', '2 - Dumping description etc', '99', '1'),
+  (NULL, '2017-07-13 12:30:10', '3', '1993', '3-documentfake', '3 - Dumping description etc', '101.20', '0');
