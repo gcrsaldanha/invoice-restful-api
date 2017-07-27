07708ecca26bd95aa4aca0ebacaea06757c0141a
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2017-07-27 13:03:12 -0300

    :hammer: Add tests for parseYear and truncate edge case to other tests

--- app/test/invoiceUtils.js
+++ app/test/invoiceUtils.js
@@ -6,16 +6,19 @@ var utils = require('../controllers/utils/InvoiceUtils');
 
 var assert = chai.assert;
 
-describe('InvoiceUtils (parsing)', function() {
+describe('InvoiceUtils (parsing)', () => {
 
-  describe('parsePage(page)', function() {
-    it('should return 1 if page is NaN', function() {
+  describe('parsePage(page)', () => {
+    it('should return 1 if page is NaN', () => {
       assert.equal(utils.parsePage('its not a number =('), 1);
     });
-    it('should return 1 if page is < 1', function() {
+    it('should return 1 if page is < 1', () => {
       assert.equal(utils.parsePage(0), 1);
     });
-    it('should return page if page is a number greater than 0', function() {
+    it('should truncate page if page is positive decimal', () => {
+      assert.equal(utils.parsePage(99.99), 99);
+    });
+    it('should return page if page is a number greater than 0', () => {
       assert.equal(utils.parsePage(10), 10);
     });
   });
@@ -32,26 +35,50 @@ describe('InvoiceUtils (parsing)', function() {
     it('should return 1 if limit < 1', () => {
       assert.equal(utils.parseLimit(0, maxLimit, defaultLimit), 1);
     });
+    it('should truncate decimal limit if and 1 <= limit <= maxLimit', () => {
+      assert.equal(utils.parseLimit(20.999, maxLimit, defaultLimit), 20);
+    });
     it('should return limit if 1 <= limit <= maxLimit', () => {
       assert.equal(utils.parseLimit(25, maxLimit, defaultLimit), 25);
     });
   });
 
   describe('parseMonth(month)', () => {
-    it('should return \'\' if month is NaN', () => {
+    it('should return empty string if month is NaN', () => {
       assert.equal(utils.parseMonth('not a number =/'), '');
     });
-    it('should return \'\' if month < 1', () => {
+    it('should return empty string if month < 1', () => {
       assert.equal(utils.parseMonth(0), '');
     });
-    it('should return \'\' if month > 12', () => {
+    it('should return empty string if month > 12', () => {
       assert.equal(utils.parseMonth(13), '');
     });
+    it('should truncate month if month is decimal between [1, 12]', () => {
+      assert.equal(utils.parseMonth(1.5), 1);
+    })
     it('should return month if month is between [1, 12]', () => {
       assert.equal(utils.parseMonth(12), 12);
       assert.equal(utils.parseMonth(1), 1);
       assert.equal(utils.parseMonth(6), 6);
     });
   });
+
+  describe('parseYear(year)', () => {
+    it('should return empty string if year is NaN', () => {
+      assert.equal(utils.parseYear('not a number =/'), '');
+    });
+    it('should return empty string if year < 0', () => {
+      assert.equal(utils.parseYear(-1), '');
+    });
+    it('should truncate year if year is positive decimal', () => {
+      assert.equal(utils.parseYear(2017.9999), 2017);
+    })
+    it('should return year if year >= 0', () => {
+      assert.equal(utils.parseYear(0), 0);
+      assert.equal(utils.parseYear(2017), 2017);
+      assert.equal(utils.parseYear(2017), 2017);
+    });
+  });
+
 
 });
